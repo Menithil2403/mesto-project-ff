40de458eb8838c973daa7391ce397a22ec8dea50
Menithil <practik.txt@yandex.kz>
2024-11-10 17:21:01 +0500

    dobavlenie_i_ydalenie

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const content = document.querySelector('.content');
+const placeList = content.querySelector('.places__list');
+
+function addSong(imgLink, cardTitle, altLink = 'test') {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    
+    placeList.append(cardElement);
+
+    cardElement.querySelector('.card__image').setAttribute('src', imgLink);
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardElement.querySelector('.card__image').setAttribute('alt', altLink); 
+    
+    placeList.onclick = function(e) {
+        const btn = e.target.closest('.card__delete-button');
+        if (!btn) {
+          return;
+        }
+        
+        btn.parentElement.remove();
+    }
+}
+
+let arrayLength = initialCards.length;
+for (let i = 0; i < arrayLength; i++) {
+    addSong(initialCards[i].link, initialCards[i].name)
+}
+
