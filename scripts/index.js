c519c3eedfdb45bcda02b943be782bbf149b82f2
Menithil <practik.txt@yandex.kz>
2024-11-15 12:55:26 +0500

    ispr:full_prod

--- scripts/index.js
+++ scripts/index.js
@@ -10,36 +10,30 @@
 const content = document.querySelector('.content');
 const placeList = content.querySelector('.places__list');
 
-function addCard(imgLink, cardTitle, altLink = 'test') {
+function addCard(imgLink, cardTitle, altText = 'test') {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-    
-    placeList.append(cardElement);
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
 
     cardElement.querySelector('.card__image').setAttribute('src', imgLink);
     cardElement.querySelector('.card__title').textContent = cardTitle;
-    cardElement.querySelector('.card__image').setAttribute('alt', altLink); 
+    cardElement.querySelector('.card__image').setAttribute('alt', altText); 
+
+    cardDeleteButton.addEventListener('click', () => {
+        deleteCard(cardElement);
+    });
+
+    return cardElement;
 }
 
-function delCard() {
-    placeList.onclick = function(e) {
-        const btn = e.target.closest('.card__delete-button');
-        if (!btn) {
-        return;
-        }
-        
-        btn.closest('.places__item').remove();
-    }
+function deleteCard(card) {
+    card.remove();
 }
 
+function renderCard(Card) {
+    placeList.prepend(Card);
+}
 
 initialCards.forEach((i) => {
-   addCard(i.link, i.name, delCard())
+    renderCard(addCard(i.link, i.name));
 })
-
-/* let arrayLength = initialCards.length;
-
-for (let i = 0; i < arrayLength; i++) {
-    addSong(initialCards[i].link, initialCards[i].name)
-} */
-
